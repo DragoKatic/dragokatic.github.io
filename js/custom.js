ef95e492002c643285aa45da4b49b827a32c5ace
Drago Katić Development <72498470+DragoKatic@users.noreply.github.com>
2025-11-13 16:16:37 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,14 @@
-
-  (function ($) {
+(function ($) {
   
   "use strict";
 
     // PRE LOADER
-    $(window).load(function(){
+  $(window).load(function(){
       $('.preloader').fadeOut(1000); // set duration in brackets    
     });
 
     // CUSTOM LINK
-    $('.custom-link').click(function(){
+  $('.custom-link').click(function(){
     var el = $(this).attr('href');
     var elWrapped = $(el);
     var header_height = $('.navbar').height() + 10;
@@ -23,33 +22,12 @@
       var totalScroll = offsetTop-navheight;
 
       $('body,html').animate({
-      scrollTop: totalScroll
+        scrollTop: totalScroll
       }, 300);
-  }
-});
- })(window.jQuery);
+    }
+  });
+})(window.jQuery);
 
 //Year in the footer
 var thisYear = new Date().getFullYear();
 document.getElementById("year").innerHTML = thisYear;
-
-/*
-//History in browser
-var index = document.location.lastIndexOf("/");
-var filename = document.location.substr(index);
-if(filename.indexOf("index.html")>-1){
-   window.history.pushState('Drago Katić', 'Portfolio', 'https://dragokatic.github.io/');
-}
-if(filename.indexOf("papers.html")>-1){
-   window.history.pushState('Drago Katić', 'Papers', 'https://dragokatic.github.io/papers.html');
-}
-if(filename.indexOf("ecommerce.html")>-1){
-   window.history.pushState('Drago Katić', 'eCommerce', 'https://dragokatic.github.io/ecommerce.html');
-}
-if(filename.indexOf("seo.html")>-1){
-   window.history.pushState('Drago Katić', 'SEO', 'https://dragokatic.github.io/seo.html');
-}
-if(filename.indexOf("websites.html")>-1){
-   window.history.pushState('Drago Katić', 'Websites', 'https://dragokatic.github.io/websites.html');
-}
-*/
---
+++ js/menu-active.js
@@ -0,0 +1,20 @@
+//menu-active
+//by ChatGPT
+
+$(document).ready(function () {
+    var path = window.location.pathname.split("/").pop(); // uzima ime fajla, npr. "seo.html"
+
+    if (path === '') {
+        path = 'index.html'; // ako je root (npr. domen bez fajla)
+    }
+
+    $('.navbar-nav .nav-link').each(function () {
+        var href = $(this).attr('href');
+
+        if (href === path) {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    });
+});
